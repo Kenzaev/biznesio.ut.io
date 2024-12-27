53aaa1d75b19b5bc411d4379c7ada73ef164c702
Kenzaev <kenjaevdodojon806@gmail.com>
2024-12-27 22:38:45 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,1 +1,18 @@
+const mysql = require('mysql2');
 
+const db = mysql.createConnection({
+    host: 'localhost', // или ваш хост
+    user: 'root', // ваш MySQL пользователь
+    password: 'root', // ваш MySQL пароль
+    database: 'shopdb.products' // имя вашей базы данных
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Ошибка подключения к MySQL:', err);
+        return;
+    }
+    console.log('Подключение к MySQL установлено успешно!');
+});
+
+module.exports = db;
