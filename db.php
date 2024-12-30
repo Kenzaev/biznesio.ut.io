00bfc9ef5701f3112bea36d7d7d7db2fc7a43af8
Kenzaev <kenjaevdodojon806@gmail.com>
2024-12-30 14:45:58 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,13 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "root";
+$password = "yourpassword"; // Замените на ваш пароль
 $dbname = "my_website_db";
 
+// Создание соединения
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Проверка соединения
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
