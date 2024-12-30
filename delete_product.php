8d44b56078e765d0687b10503a2022b8c866266f
Kenzaev <kenjaevdodojon806@gmail.com>
2024-12-30 14:39:08 +0300

    Update and rename pr to delete_product.php

---
+++ delete_product.php
@@ -0,0 +1,16 @@
+<?php
+include 'db.php';
+
+$id = $_POST['id'];
+
+$sql = "DELETE FROM products WHERE id = $id";
+
+if ($conn->query($sql) === TRUE) {
+    echo json_encode(array('message' => 'Product deleted successfully'));
+} else {
+    echo json_encode(array('error' => $conn->error));
+}
+
+$conn->close();
+?>
+
