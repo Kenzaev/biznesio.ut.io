4309fd7db3f088b1d74a0412d10e08e9821d670c
Kenzaev <kenjaevdodojon806@gmail.com>
2024-12-30 14:42:39 +0300

    Update db.php

--- db.php
+++ db.php
@@ -4,10 +4,8 @@ $username = "root";
 $password = "root";
 $dbname = "my_website_db";
 
-// Создание соединения
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Проверка соединения
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
